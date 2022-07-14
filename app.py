3fa947fc1c40a829e628b64607b5d83a9e6e38fc
Dhanun R Nair <109233637+dhanunR@users.noreply.github.com>
2022-07-14 16:22:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -81,9 +81,10 @@ if st.button(label='Click to Remove Null Values',key=3):
         # Download option
         null_values = null.to_csv(index=False).encode('utf-8')
         st.download_button(label="Download data as CSV",data= null,file_name='NullValuesRemoved.csv',mime='text/csv')
+ st.markdown("---")
         
-#Remove Null Values From Specific Column   
-if st.button(label='Click to Remove Null Values From Specific Column',key=4):
+#Remove Null Values From Specific Column  
+st.markdown('Click to Remove Null Values From Specific Column')
     with st.form(key="my_form3"):
         column = read_data.columns
         option = st.selectbox("Select the Column",column)
@@ -91,7 +92,7 @@ if st.button(label='Click to Remove Null Values From Specific Column',key=4):
         snv=read_data.dropna(axis=0, subset=[option])
     snvr=snv.to_csv(index=False).encode('utf-8')
     st.download_button(label="Download data as CSV",data=snvr,file_name='SNullValuesRemoved.csv',mime='text/csv')
-    st.markdown("---")
+st.markdown("---")
     
 # Check for duplication
 if st.button('Checking for Duplication Rate',key=5): 
