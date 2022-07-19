a149cb48ce3516f01f91380dba5f590143dc452c
Dhanun R Nair <109233637+dhanunR@users.noreply.github.com>
2022-07-19 10:53:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -179,9 +179,9 @@ st.markdown("---")
 # concatenate Two Columns
 st.markdown("Concatenate TWO columns")
 with st.form(key="new_form"):
-    read_data1 = read_data.select_dtypes(include=['object']
-    selectedcolumn = st.selectbox('Select the First Column',options=read_data1.column)
-    selectedcolumn2 = st.selectbox('Select the Second Column',options=read_data1.column)
+    #read_data1 = read_data.select_dtypes(include=['object'])
+    selectedcolumn = st.selectbox('Select the First Column',options=read_data.select_dtypes(include=['object']))
+    selectedcolumn2 = st.selectbox('Select the Second Column',options=read_data.select_dtypes(include=['object']))
     Newcolumn = st.text_input("Enter the New Column Name:")
     submit_button = st.form_submit_button(label="submit")
     read_data = read_data[selectedcolumn].astype(str) + read_data[selectedcolumn2]
