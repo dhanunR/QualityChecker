c378736b48a8edbf8256123c05f496ea2138f36f
Dhanun R Nair <109233637+dhanunR@users.noreply.github.com>
2022-07-26 09:10:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -204,13 +204,13 @@ if st.button(label='Remove special character',key=10):
               "=",">","?","@","[","\\","]","^","_",
               "`","{","|","}","~","–","$")
     #newdata = read_data
-    read_data = read_data.drop(spec_chars,axis=1,inplace=True)
+    #read_data = read_data.drop(spec_chars,axis=1,inplace=True)
     #read_data = read_data.str.replace(spec_chars,"")
-    #read_data = read_data.replace(spec_chars,"")
+    read_data1 = read_data.replace(spec_chars,"")
     #read_data = read_data.dropna(axis=0)
-    read_data = read_data.to_csv(index=False).encode('utf-8')
+    read_data2 = read_data1.to_csv(index=False).encode('utf-8')
     
-    st.download_button(label="Download data as CSV",data=read_data,file_name='Specialchar.csv',mime='text/csv')
+    st.download_button(label="Download data as CSV",data=read_data2,file_name='Specialchar.csv',mime='text/csv')
     #read_data.to_csv(r'C:\Users\217648\Downloads\file3.csv', index=False)
 st.markdown("---")
 
