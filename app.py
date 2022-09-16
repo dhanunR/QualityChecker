1fe4e3bd49e0c66ba0c9a202bb73398eb5cf6a3f
Dhanun R Nair <109233637+dhanunR@users.noreply.github.com>
2022-09-16 16:14:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,9 +23,9 @@ if upload_data is not None:
     read_data = pd.read_csv(upload_data, encoding='latin-1',on_bad_lines='skip')
 
 read_data = read_data
-    st.subheader('Data Quality Dashboard')
-    null_values = read_data.isnull().sum()/len(read_data)*100
-    st.progress(null_values)
+st.subheader('Data Quality Dashboard')
+null_values = read_data.isnull().sum()/len(read_data)*100
+st.progress(null_values)
 
 # Looking at your dataset
 st.write("Dataset Overview : ")
