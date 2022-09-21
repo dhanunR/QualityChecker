4878146742c14d31a7f414f09ff638cb0b15335c
Dhanun R Nair <109233637+dhanunR@users.noreply.github.com>
2022-09-21 11:42:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,12 +42,12 @@ except:
     st.stop()
 st.markdown("---")
 
-percentage
+#percentage
 st.write("Dataset Overview percentage: ")
 st.subheader('Data Quality percentage: ')
 null_values = read_data.isnull().sum()/len(read_data)*100
 missing = null_values.sum().round(2)
-st.progress(null_values)
+st.progress(missing)
 # Looking at your dataset
 
 # Dataset Shape
