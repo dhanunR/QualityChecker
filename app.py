6f5ae40d9dcec116dbc9e2a41b509752b2f20ae5
Dhanun R Nair <109233637+dhanunR@users.noreply.github.com>
2022-09-26 12:06:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,8 +44,8 @@ st.markdown("---")
 
 #percentage
 st.write("Dataset Overview percentage: ")
-st.subheader('Data Quality percentage: ')
-null_values = read_data.isnull().sum()/len(read_data)*100
+st.subheader('Data Null value percentage: ')
+null_values = read_data.isnull()
 #missing = null_values.sum().round(2)
 st.write(null_values)
 #st.progress(missing)
