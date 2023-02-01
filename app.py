03f0203d9a25b86cda47d47c95d65bc0b13b2bb2
Dhanun R Nair <109233637+dhanunR@users.noreply.github.com>
2023-02-01 14:16:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ import pandas as pd
 import numpy as np
 
 
+# Create SparkSession from builder
+import pyspark
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.master("local[1]") \
+                    .appName('SparkByExamples.com') \
+                    .getOrCreate()
+
 
 st.title("Quality Checker")
 st.write("This application will allow you to upload your dataset and run a quality check on it.")
